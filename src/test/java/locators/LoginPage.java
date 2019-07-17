e3f2eb3687773c9523b86bf0f81de45aa335b0a9
cesar naja <cesar_naja@live.com>
2019-07-16 22:19:58 -0500

    Just deleted a couple of 'unnecessary' things

--- src/test/java/locators/LoginPage.java
+++ src/test/java/locators/LoginPage.java
@@ -1,11 +1,10 @@
 package locators;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import utilities.Driver;
+
 
 public class LoginPage {
 
