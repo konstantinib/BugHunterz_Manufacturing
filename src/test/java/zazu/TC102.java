617e1b99f3957363f643d200229739f88725abcd
aizada malikova <zazunya5555@gmail.com>
2019-07-17 16:39:51 -0500

    tc-02 done

--- src/test/java/zazu/TC102.java
+++ src/test/java/zazu/TC102.java
@@ -8,9 +8,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import utilities.Config;
 import utilities.Driver;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class TC102 {
@@ -19,7 +21,7 @@ public class TC102 {
     static MainPage mainPage;
     static ManufacturingHomePage manufacturingHome;
 
-    @Test
+    @BeforeMethod
     public void loginAndGoToManufacturingLink()  throws InterruptedException{
         //1st Initialize the loginPage & mainPage constructor
         loginPage = new LoginPage(driver);
@@ -41,6 +43,7 @@ public class TC102 {
 
         //6th Click the "Manufacturing" link on the top navigation bar
         mainPage.getManufactoringLink().click();
+        Thread.sleep(3000);
 
 
     }
@@ -49,16 +52,36 @@ public class TC102 {
     @Test
     public void aizada_FilterButton() throws InterruptedException
     {
-////        Thread.sleep(3000);
-////        WebElement clickOnManifactoring = driver.findElement(By.linkText("Manufacturing"));
-////        clickOnManifactoring.click();
        Thread.sleep(3000);
-       WebElement manufacturingOrderLink = driver.findElement(By.xpath("//a[@data-menu='115']"));
-//        manufacturingOrderLink.click();
+       // 1- Click on second ManufacturingOrders link in Manufacturing home page
+       WebElement manufacturingOrderLink = driver.findElement(By.xpath("(//span[@class='oe_menu_text'])[19]"));
+       manufacturingOrderLink.click();
+
+       String actualMOLDisplayed = driver.findElement(By.xpath("(//span[@class='oe_menu_text'])[19]")).getText();
+       String expectedResult = "Manufacturing Orders";
+
+       // 2- Creating an object of SoftAssert
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(actualMOLDisplayed.equals(expectedResult));
+        Thread.sleep(3000);
+
+        // 3- Click on plus button, to see the filter button
+       WebElement plusButton = driver.findElement(By.xpath("//span[@title='Advanced Search...']"));
+       plusButton.click();
+
+       // 4- Checking the plus button is changed to minus
+       WebElement advanceSearch = driver.findElement(By.xpath("//span[@title='Advanced Search...']"));
+        softAssert.assertTrue(advanceSearch.getAttribute("class").contains("minus"));
+        Thread.sleep(3000);
+
+       // 5- Click on filters button
+       WebElement filtersButton = driver.findElement(By.xpath("(//button[@aria-expanded='false'])[2]"));
+       filtersButton.click();
+        WebElement filters = driver.findElement(By.xpath("(//button[@aria-expanded='false'])[2]"));
+       softAssert.assertTrue(filters.getAttribute("class").contains("toggle"));
 
 
 
-    }
+            }
 
-
-}
+        }
