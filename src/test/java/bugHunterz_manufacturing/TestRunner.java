ffb167ebf274a6d9999414914d4a83e8844d122d
konstantin <bochkarevk@yahoo.com>
2019-07-17 00:13:00 -0500

    Merge branch 'KonstantinBranch' I Had some conflict to solve # Conflicts: #	src/test/java/bugHunterz_manufacturing/TestRunner.java #	src/test/java/locators/ManufacturingHomePage.java

--- src/test/java/bugHunterz_manufacturing/TestRunner.java
+++ src/test/java/bugHunterz_manufacturing/TestRunner.java
@@ -3,11 +3,11 @@ package bugHunterz_manufacturing;
 import locators.LoginPage;
 import locators.MainPage;
 import locators.ManufacturingHomePage;
+import locators.ManufacturingReportingPage;
 import locators.ProductsPage;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
@@ -22,7 +22,9 @@ public class TestRunner {
     static LoginPage loginPage;
     static MainPage mainPage;
     static ManufacturingHomePage manufacturingHome;
+    static ManufacturingReportingPage manufacturingReportPage;
     static ProductsPage productPage;
+
 
     @BeforeMethod
     public void loginAndGoToManufacturingLink()
@@ -36,29 +38,52 @@ public class TestRunner {
         driver.manage().timeouts().implicitlyWait(5 , TimeUnit.SECONDS);
 
         //3rd Entering username
-        loginPage.getUsernameTextField().sendKeys(Config.getProperty("username"));
+        loginPage.getUsernameTextField.sendKeys(Config.getProperty("username"));
 
         //4th Entering password
-        loginPage.getPasswordTextField().sendKeys(Config.getProperty("password"));
+        loginPage.getPasswordTextField.sendKeys(Config.getProperty("password"));
 
         //5th Clicking the "Login" button
-        loginPage.getLoginButton().click();
+        loginPage.getLoginButton.click();
         driver.manage().timeouts().implicitlyWait(3 , TimeUnit.SECONDS);
 
         //6th Click the "Manufacturing" link on the top navigation bar
-        mainPage.getManufactoringLink().click();
+        mainPage.getManufacturingLink.click();
         driver.manage().timeouts().implicitlyWait(3 , TimeUnit.SECONDS);
 
     }
+
     @Test
     public void Cesar_groupByRouting() throws InterruptedException
     {
         manufacturingHome = new ManufacturingHomePage(driver);
 
-        Thread.sleep(4000);
-        //1st Click on the "Manufacturing Orders" link under "Reporting"
-        manufacturingHome.getReportingManufactoringLink().click();
+        Actions actions = new Actions(driver);
 
+
+        Thread.sleep(3000);
+        //1st Click on the "Manufacturing Orders" link under "Reporting"
+        actions.moveToElement(manufacturingHome.getReportingManufacturingLink).click().perform();
+        //manufacturingHome.getReportingManufacturingLink.click();
+
+        //2nd Click on the "Advanced Search" link on the search bar
+        manufacturingReportPage = new ManufacturingReportingPage(driver);
+        Thread.sleep(3000);
+        actions.moveToElement(manufacturingReportPage.getAdvancedSearchButton).click().perform();
+
+        //3rd Click on the "Group By" button
+        actions.moveToElement(manufacturingReportPage.getGroupByButton).click().perform();
+
+        //4th Click on the "Routing" link in the drop down menu
+        Thread.sleep(1000);
+        actions.moveToElement(manufacturingReportPage.getRoutingLink).click().perform();
+        driver.manage().timeouts().implicitlyWait(3 , TimeUnit.SECONDS);
+
+
+        //5th Once we click on the "Routing" link, we should assert that new graph is displayed
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(manufacturingReportPage.getGraph.isDisplayed(), "Graph was not displayed!");
+        System.out.println("Cesar's Method Finished With No Problems");
     }
 
     @Test
@@ -84,5 +109,14 @@ public class TestRunner {
         softAssert.assertTrue(productPage.filtersDropDownMenu.isDisplayed(), "Drop down menu is not displayed!");
 
     }
+    @AfterMethod
+    public void close()
+    {
+        driver.close();
+        driver = null;
+    }
+
+
+
 
 }
---
+++ src/test/java/locators/ManufacturingReportingPage.java
@@ -0,0 +1,43 @@
+package locators;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ManufacturingReportingPage {
+
+    public WebDriver driver;
+
+    public ManufacturingReportingPage(WebDriver driver)
+    {
+        this.driver = driver;
+        PageFactory.initElements(driver , this);
+    }
+
+    @FindBy(css = ".o_searchview_more.fa.fa-search-plus")
+    public WebElement getAdvancedSearchButton;
+
+    @FindBy(xpath = "//div[@class = 'btn-group o_search_options']/div[2]/button")
+    public WebElement getGroupByButton;
+
+    @FindBy(linkText = "Routing")
+    public WebElement getRoutingLink;
+
+    @FindBy(xpath = "//div[@class='o_graph']")
+    public WebElement getGraph;
+
+
+ /* By advancedSeachButton = By.cssSelector(".o_searchview_more.fa.fa-search-plus");
+    By groupByButton = By.xpath();
+    By routingLink = By.linkText("Routing");
+    public WebElement getAdvancedSearchButton()
+    {
+        return driver.findElement(advancedSeachButton);
+    }
+    public WebElement getGroupByButton()
+    {
+        return driver.findElement(groupByButton);
+    }
+    public WebElement getRoutingLink(){ return  driver.findElement(routingLink); }*/
+}
